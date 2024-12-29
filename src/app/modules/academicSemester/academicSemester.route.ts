8bf13d27ea446b290740a664bbd8bc8cd3d67118
Md. Shariful Islam <freelancershariful10@gmail.com>
2024-12-29 16:58:19 +0600

    Create semester ragistration

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -2,26 +2,45 @@ import express from "express";
 import validateRequest from "../../middlewares/validateRequest";
 import { AcademicSemesterControllers } from "./academicSemester.controller";
 import { AcademicSemesterValidation } from "./academicSemester.validation";
+import auth from "../../middlewares/auth";
+import { USER_ROLE } from "../user/user.const";
 
 const router = express.Router();
 
 router.post(
   "/create-academic-semester",
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(
     AcademicSemesterValidation.createAcademicSemesterValidationSchema,
   ),
   AcademicSemesterControllers.createAcademicSemester,
 );
 
-router.get("/", AcademicSemesterControllers.getAllAcademicSemesters);
+router.get(
+  "/",
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  AcademicSemesterControllers.getAllAcademicSemesters,
+);
 
 router.get(
   "/:semesterId",
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
   AcademicSemesterControllers.getSingleAcademicSemester,
 );
 
 router.patch(
   "/:semesterId",
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(
     AcademicSemesterValidation.updateAcademicSemesterValidationSchema,
   ),
