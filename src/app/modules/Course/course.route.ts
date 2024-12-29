9709069c0aefd8df7fca4050f2d63c95cd4f875a
Md. Shariful Islam <freelancershariful10@gmail.com>
2024-12-29 16:33:01 +0600

    Create course

--- src/app/modules/Course/course.route.ts
+++ src/app/modules/Course/course.route.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import auth from "../../middlewares/auth";
 import validateRequest from "../../middlewares/validateRequest";
+import { USER_ROLE } from "../user/user.const";
 import { CourseControllers } from "./course.controller";
 import { CourseValidations } from "./course.validation";
 
@@ -8,37 +9,55 @@ const router = express.Router();
 
 router.post(
   "/create-course",
-  auth("admin"),
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(CourseValidations.createCourseValidationSchema),
   CourseControllers.createCourse,
 );
 
 router.get(
   "/",
-  auth("admin", "faculty", "student"),
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
   CourseControllers.getAllCourses,
 );
 
 router.get(
   "/:id",
-  auth("admin", "faculty", "student"),
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
   CourseControllers.getSingleCourse,
 );
 
-router.delete("/:id", auth("admin"), CourseControllers.deleteCourse);
+router.delete(
+  "/:id",
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  CourseControllers.deleteCourse,
+);
 
-router.patch("/:id", auth("admin"), CourseControllers.updateCourse);
+router.patch(
+  "/:id",
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  CourseControllers.updateCourse,
+);
 
 router.put(
   "/:courseId/assign-faculties",
-  auth("admin"),
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(CourseValidations.facultiesWithCourseValidationSchema),
   CourseControllers.assignFacultiesWithCourse,
 );
 
 router.delete(
   "/:courseId/remove-faculties",
-  auth("admin"),
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(CourseValidations.facultiesWithCourseValidationSchema),
   CourseControllers.removeFacultiesFromCourse,
 );
